4c0f4b4c3eccd3fa02b8474a48ae8d7cc761381a
Brendan Ang <brendanawjang@gmail.com>
2019-12-19 21:51:17 +0800

    updated static root

--- django_anitrack/settings.py
+++ django_anitrack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+&7nk355c&q7ts2a8l^wsfoortj6cu^rn1os=t@bikt2q%^w1s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'main:main-home'
 LOGIN_URL = 'login'
