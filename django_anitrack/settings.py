6c4a03598a0bf70963b0a0268419c16cf29c0e85
Brendan Ang <brendanawjang@gmail.com>
2019-12-19 22:49:32 +0800

    update settings

--- django_anitrack/settings.py
+++ django_anitrack/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+&7nk355c&q7ts2a8l^wsfoortj6cu^rn1os=t@bikt2q%^w1s'
+#SECRET_KEY = '+&7nk355c&q7ts2a8l^wsfoortj6cu^rn1os=t@bikt2q%^w1s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anitrack.herokuapp.com']
 
 
 # Application definition
